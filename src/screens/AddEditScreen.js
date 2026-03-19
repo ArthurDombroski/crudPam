01c801e1799630d016e403fbe76b6d7928c60a8c
ArthurDombroski <tutudombroski@gmail.com>
2026-03-19 09:35:52 -0300

    feat: AddEditScreen finalizada

---
+++ src/screens/AddEditScreen.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, Button } from "react-native";
+
+import styles from "../styles/styles.js";
+
+import peopleCrud from "../servers/peopleCrud.js";
+import { TextInput } from "react-native-gesture-handler";
+
+export default function AddEditScreen({ route, navigation }) {
+
+    const person = route.params?.person;
+
+    const [firstName, setFirstName] = useState(person?.firstName || "");
+    const [lastName, setLastName] = useState(person?.lastName || "");
+    const [email, setEmail] = useState(person?.email || "");
+
+    async function save() {
+
+        const data = { firstName, lastName, email };
+
+        if (person) {
+            await peopleCrud.putPerson(person.id, data);
+        } else {
+            await peopleCrud.postPerson(data);
+        }
+    }
+
+    navigation.goBack();
+
+    return (
+        <View style={styles.container}>
+
+            <TextInput
+                placeholder="First Name"
+                value={firstName}
+                onChangeText={setFirstName}
+            />
+
+            <TextInput
+                placeholder="Last Name"
+                value={lastName}
+                onChangeText={setLastName}
+            />
+
+            <TextInput
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+            />
+
+            <Button
+                title="Salvar"
+                onPress={save}
+            />
+
+            <Button
+                title="Cancelar"
+                onPress={() => navigation.goBack()}
+            />
+        </View>
+    )
+}
