98c9cbb9719fff3d91ffc670ce5fba2ca51f283f
ArthurDombroski <tutudombroski@gmail.com>
2026-03-19 09:07:27 -0300

    feat: homeScreen finalizada

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, Button } from "react-native";
+
+import styles from "../styles/styles.js";
+
+import { getPeople, deletePerson } from "../servers/peopleCrud.js";
+
+export default function HomeScreen({ navigation }) {
+    const [people, setPeople] = useState([]);
+
+    async function loadPeople() {
+        const data = await getPeople();
+
+        setPeople(data);
+    }
+
+    useEffect(() => {
+        loadPeople();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+
+            <Text style={style.title}>Pessoas</Text>
+
+            <Button title="Adicionar Pessoa"
+                onPress={() => navigation.navigate("AddEdit")} />
+
+            <FlatList
+                data={people}
+                keyExtractor={(item) => item.id.toString()}
+
+                renderItem={({ item }) => (
+                    <CardPersonal
+                        item={item}
+                        navigation={navigation}
+                        refresh={loadPeople}
+                    />
+                )}
+            />
+
+
+        </View>
+    )
+}
