103dbce42c945f015f111d9154864f6a119ff03b
ArthurDombroski <tutudombroski@gmail.com>
2026-03-19 09:27:39 -0300

    feat: navegação entre telas configurada

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from "./src/screens/HomeScreen.js";
+import AddEditScreen from "./src/screens/AddEditScreen.js";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "HomeScreen">
+        <Stack.Screen name = "HomeScreen" component = {HomeScreen} options = {{headerShown: false}} />
+        <Stack.Screen name = "AddEditScreen" component = {AddEditScreen} options = {{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
